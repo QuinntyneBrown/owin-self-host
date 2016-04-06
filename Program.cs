308d6fbe6f22a799b2b89321d53efb29b732a72d
Quinntyne Brown <quinntynebrown@gmail.com>
2016-04-06 08:08:26 -0400

    initial code

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Owin.Hosting;
+
+namespace Chloe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddress = "http://localhost:9002/";
+
+            //WebApp.Start<Startup>(url: baseAddress);
+
+            Console.ReadLine();
+        }
+    }
+}
